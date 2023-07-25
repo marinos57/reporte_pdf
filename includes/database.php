509d84e842a32a737db4eca18a252fefb9eef604
marino5710 <brianmarin4@gmail.com>
2023-07-25 09:45:07 -0600

    SE CORRIGIO

--- includes/database.php
+++ includes/database.php
@@ -4,8 +4,8 @@ try {
     $host = $_ENV['DB_HOST'];
     $service = $_ENV['DB_SERVICE'];
     $server = $_ENV['DB_SERVER'];
-    $user = $_SESSION['DB_SERVER'];
-    $pass = $_SESSION['DB_SERVER'];
+    $user = $_ENV['DB_USER'];
+    $pass = $_ENV['DB_PASS'];
     $database = $_ENV['DB_NAME'];
 
     $db =  new PDO("informix:host=$host; service=$service;database=$database; server=$server; protocol=onsoctcp;EnableScrollableCursors=1", "$user", "$pass");
